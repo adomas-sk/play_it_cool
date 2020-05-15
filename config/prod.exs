9e439d60822a0e2c67d78bf44197e7c65d306f14
Adomas Skliutas <adomasskliutaslt@gmail.com>
2020-05-16 01:37:24 +0300

    changed prod url

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :play_it_cool_web, PlayItCoolWeb.Endpoint,
-  url: [host: "localhost", port: 80],
+  url: [host: "elimemorate.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
--- terraform/playitcool-1.tf
+++ terraform/playitcool-1.tf
@@ -21,20 +21,25 @@ resource "digitalocean_droplet" "playitcool-1" {
     inline = [
       "export PATH=$PATH:/usr/bin",
       "sudo apt-get upgrade -y",
-      "sudo apt-get update",
-      "sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common git",
+      "sudo apt-get update -y",
+      "sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common git -y",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
       "sudo apt-key fingerprint 0EBFCD88",
       "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
-      "sudo apt-get update",
-      "sudo apt-get install docker-ce docker-ce-cli containerd.io",
+      "sudo apt-get update -y",
+      "sudo apt-get install docker-ce docker-ce-cli containerd.io -y",
       "mkdir /app && cd /app",
       "git clone https://github.com/SnakesCantWearHats/play_it_cool.git .",
       "SECRET_KEY_BASE=$(date +%s | sha256sum | base64 | head -c 32 ; echo)",
       "TOKEN_SECRET=$(date +%s | sha256sum | base64 | head -c 32 ; echo)",
-      "DATABASE_URL=$(echo ${digitalocean_database_cluster.default_postgres.uri} | sed \"postgres/ecto\")",
+      "RAW_DATABASE_URL=\"${digitalocean_database_cluster.default_postgres.uri}\"",
+      "echo $TOKEN_SECRET",
+      "echo $SECRET_KEY_BASE",
+      "echo $RAW_DATABASE_URL",
+      "DATABASE_URL=\"ecto$(echo $RAW_DATABASE_URL | tail -c 11 | head -c -17)\"",
+      "echo $DATABASE_URL",
       "docker build . -t playitcool --build-arg DATABASE_URL=$DATABASE_URL --build-arg PORT=80 --build-arg SECRET_KEY_BASE=$SECRET_KEY_BASE --build-arg TOKEN_SECRET=$TOKEN_SECRET",
-      "docker run -p 127.0.0.1:80:80/tcp playitcool"
+      "sudo docker run -p 127.0.0.1:80:80/tcp playitcool"
     ]
   }
 }
